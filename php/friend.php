b188b22d6d351d91ce9672e11416ff22f96198db
B_Bro <bbrotherkk@gmail.com>
2017-09-24 16:14:40 +0900

    friend

--- php/friend.php
+++ php/friend.php
@@ -1,7 +1,6 @@
 <?php 
 	ini_set('display_errors', '1');
 	session_start();
-	session_cache_expire(14400);
 	include_once("config.php");
 	$choice = $_POST['choice'];
 	$user_iden = isset($_POST['user_iden']) ? $_POST['user_iden'] : "";
@@ -9,15 +8,12 @@
 	$friend_iden = isset($_POST['friend_iden']) ? $_POST['friend_iden'] : "";
 	$friend_id = isset($_POST['friend_id']) ? $_POST['friend_id'] : "";
 	$token = isset($_POST['token']) ? $_POST['token'] : "";
-	if (!$token == $_SESSION['token']){
-		echo "problem";
-	}
 
 	if ($choice == 1){
 		$query = "select friend.my_iden, list.iden, list.id, list.photo_uri, list.ment from user_list as list join friend_list as friend ON friend.friend_iden = list.iden where my_iden = '$user_iden'";
 /*		echo $query;
 		*/
-
+	
 		$result = mysqli_query($connect,$query)or die ("입력 실패");
 		// $data = mysqli_fetch_array($result);
 		$arr = array();
@@ -31,12 +27,66 @@
 		$json = json_encode($arr);
 		echo $json;
 	}
+	//친구추가
 	else if ($choice == 2){
-		$query = "insert into friend_list (my_iden,my_id,friend_iden,friend_id) values('$user_iden','$user_id','friend_iden','friend_id')";
-		$result = mysqli_query($query);
-		$data = mysqli_fetch_array($result);
+		//친구 아이디 유무 판단
+		$query = "select * from friend_list where my_iden ='$user_iden' and friend_id = '$friend_id'";
+		$result = mysqli_query($connect,$query);
+		$count_check = mysqli_num_rows($result);
+		// echo $query;
+		$query = "select * from user_list where id = '$friend_id'";
+		// echo $query;
+		$result = mysqli_query($connect,$query);
+		$row = mysqli_fetch_array($result);
+		$friend_iden = $row['iden'];
+		$count = mysqli_num_rows($result);
+		if ($count == 0){
+			echo "cantfind";
+			exit();
+		}
+		else if ($count_check != 0){
+			echo "already";
+			exit();
+		}
+		else {
+			$query = "insert into friend_list (my_iden,my_id,friend_iden,friend_id) values ('$user_iden','$user_id','$friend_iden','$friend_id')";
+			// echo $query . "    ||    ";
+			$result = mysqli_query($connect,$query);
+			$query = "select friend.my_iden, list.iden, list.id, list.photo_uri, list.ment from user_list as list join friend_list as friend ON friend.friend_iden = list.iden where my_iden = '$user_iden' order by list.id asc";
+			// echo $query;
+			$result = mysqli_query($connect,$query);
+			$count = mysqli_num_rows($result);
+			// echo $query;
+
+			$arr = array();	
+			for ($i=0; $i < $count; $i++) { 
+				$data = mysqli_fetch_assoc($result)or die("추출실패~~");
+				array_push($arr, $data);
+
+			}	
+
+			$json = json_encode($arr);
+			// echo $friend_iden ." || ". $friend_id;
+			
+			echo $json;
+		}
 	}
 	else if ($choice == 3){
-		echo "error";
+		$query = "delete from friend_list where my_iden = '$user_iden' and friend_iden = '$friend_iden'";
+		$result = mysqli_query($connect,$query);
+		// echo $query;
+		$query = "select friend.my_iden, list.iden, list.id, list.photo_uri, list.ment from user_list as list join friend_list as friend ON friend.friend_iden = list.iden where my_iden = '$user_iden' order by list.id asc";
+		$result = mysqli_query($connect,$query);
+		
+		$count = mysqli_num_rows($result);
+			// echo $query;
+		$arr = array();	
+		for ($i=0; $i < $count; $i++) { 
+			$data = mysqli_fetch_assoc($result)or die("추출실패~~");
+			array_push($arr, $data);
+
+		}	
+		$json = json_encode($arr);
+		echo $json;
 	}
 ?>
