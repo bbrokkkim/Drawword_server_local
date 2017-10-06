c3512c8a0bfa7d433602f9bd1258534292364888
B_Bro <bbrotherkk@gmail.com>
2017-10-07 06:06:30 +0900

    tcp 방구분 끝

--- java/Server.java
+++ java/Server.java
@@ -9,13 +9,13 @@ import java.util.List;
 
 public class Server {
     static ArrayList<ConnectionToClient> clients = new ArrayList<>();
-
+    static ArrayList<Client_info> client_list = new ArrayList<>();
     public static void main(String[] args) {
         try {
-            System.out.println("시작!!");
-            ServerSocket server = new ServerSocket(8000);
-            Socket socket = null;
+            System.out.println("시작!!");      
             System.out.println("대기중....");
+            Socket socket = null;
+            ServerSocket server = new ServerSocket(8000);
             while ((socket = server.accept()) != null) {
                 System.out.println(socket.getInetAddress() + " 로부터 연결요청이 들어왔습니다.");
                 new ServerThread(socket).start();
@@ -24,48 +24,121 @@ public class Server {
         } catch (Exception e) {
         }
     }
-
+                /*room_num = bufferedReader.readLine();
+                room_name = bufferedReader.readLine();
+                user_iden = bufferedReader.readLine();
+                user_name = bufferedReader.readLine();
+                System.out.println(room_num);
+                System.out.println(room_name);
+                System.out.println(user_iden);
+                System.out.println(user_name);*/
     public static class ServerThread extends Thread {
         Socket socket;
         ConnectionToClient conToClient;
+        Client_info client_info;
+        String room_num,room_name,user_iden,user_name;
+        // String[4] info = 
         ServerThread(Socket socket) {
             this.socket = socket;
             this.conToClient = new ConnectionToClient(socket);
             clients.add(conToClient);
+            
         }
         public void run() {
             try {
+                int ment_num = 0;
+                int info = 0;
+                boolean ment = true;
                 String input ="";
+                System.out.println("확인");
+                String room_num = "";
+                String user_name = "";
+                System.out.println(room_num+"번방");
+
+                
                 // String input = new String(buf, "UTF-8");
                 while( (input = conToClient.read())!=null){
                     // System.out.println(input);
                     //conToClient.write(input);
-                    System.out.println("message: " + input);
+                    if (ment == true){
+                        int idx = input.indexOf("《");
+                        room_num = input.substring(0,idx);
+                        user_name = input.substring(idx + 1);
+                        System.out.println( room_num + "번방을 입장했다.");
+                        System.out.println( user_name);
+                        client_info = new Client_info(socket,room_num,user_name);
+                        client_list.add(client_info);
+                            
+                        /*if (info ==0 ){
+                            room_num = input;
+                            info = 1;
+                        }
+                        else if (info == 1){
+                            user_name = input;
+                            
+                            // String asdf = client_list.get(0).getRoomNum();
+                            System.out.println( room_num + "번방을 입장했다.");*/
+                            ment = false;
+                        // }
+                    }
+                    
 
-                if (input.equals("김경관")){
-                    System.out.println("맞음");
-                }
-                else
-                    System.out.println("틀림");
+                    else {
+                        if (input.equals("fin!@#!@#")){
+                            System.out.println("사용자 나감");
+                            break;
+                        }
 
-                    sendToAll(input+"\n");
+                        int idx = input.indexOf("《");
+                        room_num = input.substring(0,idx);
+                        user_name = input.substring(idx + 1);
+                        
+                        System.out.println("number: " + room_num + " || message: " + user_name);
+                        sendToAll(user_name+ "\n",room_num);
+                    }
                 }
             } catch (Exception e) {
             }
         }
 
-        public void sendToAll(String message){
+        public void sendToAll(String message,String room_num){
             /*for(ConnectionToClient client :clients){
                 client.write(message+"\n");
                 System.out.println(clients);
                 
             }*/
-            for (int i = 0 ; i < clients.size() ; i ++ ) {
-                clients.get(i).write(message);
+            System.out.println("test" );
 
-                System.out.println(clients.get(i));
+            for (int i = 0 ; i < client_list.size() ; i ++ ) {
+                System.out.println("비교 : " + room_num + "||" + client_list.get(i).getRoomNum());
+                if ( room_num.equals( client_list.get(i).getRoomNum() )){
+                    // System.out.println("맞음");
+                    clients.get(i).write(message);
+                }
+                // System.out.println(clients.get(i));
                 
             }
+        }
+    }
+
+    public static class Client_info {
+        Socket socket;
+        String room_num,user_name;
+
+        Client_info(Socket socket, String room_num, String user_name){
+            this.socket = socket;
+            this.room_num = room_num;
+            this.user_name = user_name;
+        }
+        public void setRoomNum(String room_num) {
+            this.room_num = room_num;
+        }
+
+        public String getUserName(){
+            return user_name;
+        }
+        public String getRoomNum(){
+            return room_num;
         }
     }
 
@@ -74,7 +147,7 @@ public class Server {
         BufferedReader br;
         PrintWriter printWriter;
         ObjectOutputStream oos;
-
+        String room_num;
         ConnectionToClient(Socket socket) {
             this.socket = socket;
             try {
@@ -85,6 +158,9 @@ public class Server {
                System.out.println(e);
             }
         }
+
+
+
         public String read(){
             try{
                 return br.readLine();
@@ -108,4 +184,5 @@ public class Server {
         }
     }
 }
+
 
--- php/gamelist.php
+++ php/gamelist.php
@@ -1,4 +1,5 @@
 <?php 
+ini_set('display_errors', '1');
 	include_once("config.php");
 
 	$choice = $_POST['choice'];
@@ -7,10 +8,39 @@
 	$room_name = isset($_POST['room_name']) ? $_POST['room_name'] : "";
 
 
+	//방 생성
 	if ($choice == 1){
-		$query = "insert into room_info (room_num,room_status,today,del_status) values ('$room_name','wait','','live')";
-	}
-	//투데이 만들기1
+		date_default_timezone_set("Asia/Seoul");
+		$date_ = DATE("Y-m-d-H-i D");
+		$date = (String) $date_;
+		
+		if ( $room_name == ""){
+			$room_name = "재밌는 게임 해요!";
+		}
 
+		$query = "insert into room_info (room_name,room_status,del_status,today) values ('$room_name','wait','live','$date')";
+		echo $query;
+		$result = mysqli_query($connect,$query);
+	}
+	else if ($choice == 2){
+		$query = "select * from room_info where room_status = 'wait' and del_status = 'live'";
+		// echo $query;
+		$result = mysqli_query($connect, $query);
+		$count = mysqli_num_rows($result);
+		if ($count == 0){
+			echo "nothing";
+			exit;
+		}
+		$arr = array();
+		for ($i = 0; $i < $count; $i++) {
+			$data = mysqli_fetch_assoc($result)or die("추출실패");
+			array_push($arr, $data);
+		}
+		$json = json_encode($arr);
+		echo $json;
+	}
+
+
+	
 
 ?>
