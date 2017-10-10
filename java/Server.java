717f4b4c8dd8c22bdc890e93cb858edcb7e9420a
B_Bro <bbrotherkk@gmail.com>
2017-10-11 00:57:46 +0900

    tcp 진행중

--- java/Server.java
+++ java/Server.java
@@ -50,10 +50,9 @@ public class Server {
                 int info = 0;
                 boolean ment = true;
                 String input ="";
-                System.out.println("확인");
                 String room_num = "";
                 String user_name = "";
-                System.out.println(room_num+"번방");
+                String tcp_type = "";
 
                 
                 // String input = new String(buf, "UTF-8");
@@ -64,9 +63,9 @@ public class Server {
                         int idx = input.indexOf("《");
                         room_num = input.substring(0,idx);
                         user_name = input.substring(idx + 1);
-                        System.out.println( room_num + "번방을 입장했다.");
-                        System.out.println( user_name);
-                        client_info = new Client_info(socket,room_num,user_name);
+                        System.out.println( room_num + "번방으로.");
+                        System.out.println( user_name + "님이 입장하였습니다.");
+                        client_info = new Client_info(socket,room_num,user_name,"wait");
                         client_list.add(client_info);
                             
                         /*if (info ==0 ){
@@ -78,68 +77,131 @@ public class Server {
                             
                             // String asdf = client_list.get(0).getRoomNum();
                             System.out.println( room_num + "번방을 입장했다.");*/
-                            ment = false;
+                        ment = false;
                         // }
                     }
                     
 
                     else {
-                        if (input.equals("fin!@#!@#")){
-                            System.out.println("사용자 나감");
+                        if (input.equals("fin!@#!@#》")){
+                            exit_room(user_name);
                             break;
                         }
-
+                        System.out.println(input);
                         int idx = input.indexOf("《");
+                        tcp_type = input.substring(0,idx);
+                        System.out.println(tcp_type);
+                        
+
+                        input = input.substring(idx + 1);
+                        System.out.println(input);
+                            
+                        idx = input.indexOf("《");
                         room_num = input.substring(0,idx);
                         user_name = input.substring(idx + 1);
-                        
-                        System.out.println("number: " + room_num + " || message: " + user_name);
-                        sendToAll(user_name+ "\n",room_num);
+                        System.out.println(room_num);
+                        System.out.println(user_name);
+
+
+                        if (tcp_type.equals("1")){
+                            System.out.println("number: " + room_num + " || message: " + user_name);
+                            sendToAll("《1《"+user_name+ "\n",room_num);
+                        }
+                        else if (tcp_type.equals("2")){
+                            user_ready(user_name);
+                            String user_list = check_user_list(room_num);
+                            System.out.println(user_list);
+                            
+                            sendToAll("《2"+user_list+ "\n",room_num);
+                        }
+                        else if (tcp_type.equals("3")){
+                            System.out.println("ㅁㄴㅇㄹ");
+                        }
+                        else 
+                            System.out.println("aaa");
                     }
                 }
             } catch (Exception e) {
             }
         }
 
-        public void sendToAll(String message,String room_num){
-            /*for(ConnectionToClient client :clients){
-                client.write(message+"\n");
-                System.out.println(clients);
-                
-            }*/
-            System.out.println("test" );
+        public void exit_room(String user_name){
+            for (int i =0 ;i < client_list.size() ; i ++) {
+                if (user_name.equals(client_info.getUserName())){
+                    System.out.println(client_list.get(i).getUserName() + " 나감");
+                    client_list.remove(i);
+                    clients.remove(i);
+                }
+            }
+        }
 
+        public void user_ready(String user_name) {
+            for (int i =0 ;i < client_list.size() ; i ++) {
+                if (user_name.equals(client_info.getUserName())){
+                    if (client_list.get(i).getReadycheck().equals("wait")) {
+                        client_list.get(i).setReadycheck("ready");
+                    }
+                    else
+                        client_list.get(i).setReadycheck("wait");
+                
+                    return;
+                }
+            }
+        }
+        
+        public void sendToAll(String message,String room_num){
+            // System.out.println("test");
+            System.out.println("사이즈" + client_list.size());
             for (int i = 0 ; i < client_list.size() ; i ++ ) {
-                System.out.println("비교 : " + room_num + "||" + client_list.get(i).getRoomNum());
+                // System.out.println("비교 : " + room_num + "||" + client_list.get(i).getRoomNum());
                 if ( room_num.equals( client_list.get(i).getRoomNum() )){
                     // System.out.println("맞음");
                     clients.get(i).write(message);
                 }
                 // System.out.println(clients.get(i));
-                
             }
+        }
+
+        public String check_user_list(String room_num){
+            String user_list_json = "";
+            for (int i = 0 ; i < client_list.size() ; i ++ ){
+                if ( room_num.equals( client_list.get(i).getRoomNum() )){
+                    
+                    user_list_json = user_list_json + "《" + client_list.get(i).getUserName() + "》" + client_list.get(i).getReadycheck();
+
+                    
+                }
+            }
+            return user_list_json;
         }
     }
 
     public static class Client_info {
         Socket socket;
-        String room_num,user_name;
+        String room_num,user_name,readycheck;
 
-        Client_info(Socket socket, String room_num, String user_name){
+        Client_info(Socket socket, String room_num, String user_name, String readycheck){
             this.socket = socket;
             this.room_num = room_num;
             this.user_name = user_name;
-        }
-        public void setRoomNum(String room_num) {
-            this.room_num = room_num;
+            this.readycheck = readycheck;
         }
 
+        //setter
+        public void setReadycheck(String readycheck) {
+            this.readycheck = readycheck;
+        }
+
+        //getter
         public String getUserName(){
             return user_name;
         }
         public String getRoomNum(){
             return room_num;
         }
+        public String getReadycheck(){
+            return readycheck;
+        } 
     }
 
     public static class ConnectionToClient {
